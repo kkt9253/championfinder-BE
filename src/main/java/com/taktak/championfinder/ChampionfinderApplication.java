4e24806b4a096d0e0a2458b7abab1c0827555efd
kkt9253 <kkt9253@naver.com>
2024-09-06 23:49:40 +0900

    CREATE : 롤_챔피언_추천_프로젝트 세팅

---
+++ src/main/java/com/taktak/championfinder/ChampionfinderApplication.java
@@ -0,0 +1,13 @@
+package com.taktak.championfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taktak/championfinder/ChampionfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taktak.championfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
